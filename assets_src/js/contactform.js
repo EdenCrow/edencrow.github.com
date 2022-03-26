a20351f941cae9a4cae833e3d2c6dd9684e09433
Eden Crow <731483+EdenCrow@users.noreply.github.com>
2022-03-26 00:49:50 +0000

    Contact form client-side validation cleanup

--- assets_src/js/contactform.js
+++ assets_src/js/contactform.js
@@ -1,35 +1,32 @@
 import isEmail from "validator/lib/isEmail";
 
 // Set div IDs
+// (note IDs must match name attribute for form and submit)
 const formID = "formData";
 const submitID = "submit";
 const captchaID = "captcha";
-
-// Set if checking email and email field id
-const checkEmail = true;
 const emailID = "email";
 
 // Set CSS classes
-const textErrorClass = "errorText";
 const inputErrorClass = "blinkBorder";
 const captchaInputErrorClass = "captchaBlinkBorder";
 
-// Set changeable vars
-const processingMessage =
-  '<i class="fa fa-spinner" aria-hidden="true"></i> Processing';
+// Set loading icon
+const spinner = '<i class="fa fa-spinner" aria-hidden="true"></i>';
 
 // Setup captcha array for checking against
 const captchaArray = ["h-captcha-response", "g-recaptcha-response", captchaID];
 
 // Setup classes for form elements
 class Button {
-  constructor(id, processingText) {
+  constructor(id) {
     this.element = document.getElementById(id);
-    this.processingText = processingText;
+    this.spinner = spinner;
     this.processedText = this.element.innerHTML;
   }
-  processing() {
-    this.element.innerHTML = this.processingText;
+  processing(validating = false) {
+    let text = validating ? "Validating" : "Processing";
+    this.element.innerHTML = [this.spinner, text].join(" ");
     this.element.style.cursor = "default";
     this.element.disabled = true;
   }
@@ -41,30 +38,22 @@ class Button {
 }
 
 class Input {
-  constructor(id) {
+  constructor(id, isCaptcha = false) {
     this.element = document.getElementById(id);
     this.isError = false;
-    if (captchaArray.includes(id)) {
-      this.isCaptcha = true;
-      this.errorClass = captchaInputErrorClass;
-    } else {
-      this.isCaptcha = false;
-      this.errorClass = inputErrorClass;
-    }
+    this.isCaptcha = isCaptcha;
+    this.errorClass = this.isCaptcha ? captchaInputErrorClass : inputErrorClass;
 
     // Setup error message text
     this.errorTextID = "errorText" + id;
-    this.textErrorClass = textErrorClass;
-    if (this.isCaptcha) {
-      this.textErrorClass += "--captcha";
-    }
-    this.errorMessageDefault = "<br/>";
+    this.errorMessageDefault = "<br>";
     this.element.insertAdjacentHTML(
       "afterend",
-      `<div id="${this.errorTextID}" class="${this.textErrorClass}">${this.errorMessageDefault}</div>`
+      `<div id="${this.errorTextID}">${this.errorMessageDefault}</div>`
     );
     this.errorMessageDiv = document.getElementById(this.errorTextID);
 
+    // Function to remove errors
     this.removeError = function () {
       this.element.classList.remove(this.errorClass);
       this.errorMessageDiv.innerHTML = this.errorMessageDefault;
@@ -117,19 +106,19 @@ class Input {
 
 // Get form and submit
 const form = document.getElementById(formID);
-const submitButton = new Button(submitID, processingMessage);
+const submitButton = new Button(submitID);
 
 // Setup captcha
-const captcha = new Input(captchaID);
+window.captchaInput = new Input(captchaID, true);
 window.captchaCallback = function () {
-  if (captcha.isError) {
-    captcha.removeError();
+  if (captchaInput.isError) {
+    captchaInput.removeError();
   }
 };
 
 // Setup input classes
 const formFields = new FormData(form);
-let fieldClasses = [];
+let fieldClasses = [captchaInput];
 for (let pair of formFields.entries()) {
   let field = pair[0];
   if (!captchaArray.includes(field)) {
@@ -144,16 +133,11 @@ function checkEmpty(formData) {
   let emptyValues = [];
   for (let pair of formData.entries()) {
     let inputKey = pair[0];
-    let inputClassName = pair[0] + "Input";
     let inputValue = pair[1].trim();
-
     if (inputValue.length === 0) {
       emptyValues.push(inputKey);
-      if (!captchaArray.includes(inputKey)) {
-        window[inputClassName].error(0);
-      } else {
-        captcha.error(0);
-      }
+      let inputClassName = inputKey + "Input";
+      window[inputClassName].error(0);
     }
   }
   return emptyValues;
@@ -179,22 +163,26 @@ form.addEventListener(submitID, (event) => {
   fieldClasses.forEach((item) => {
     item.reset();
   });
-  captcha.reset();
 
   // Change button
-  submitButton.processing();
+  submitButton.processing(true);
 
   // Get form data
   let formData = new FormData(form);
+  formData.delete("g-recaptcha-response");
+  for (let pair of formData.entries()) {
+    if (captchaArray.includes(pair[0])) {
+      formData.delete(pair[0]);
+      formData.append("captcha", pair[1]);
+    }
+  }
 
   // Check if data is empty
   let emptyValues = checkEmpty(formData);
 
   // Check if email is valid if email field present
   let validEmail = false;
-  if (!checkEmail) {
-    validEmail = true;
-  } else if (!emptyValues.includes(emailID)) {
+  if (!emptyValues.includes(emailID)) {
     validEmail = validateEmail(formData.get(emailID));
   }
 
@@ -205,5 +193,5 @@ form.addEventListener(submitID, (event) => {
   }
 
   // Input is valid
-  console.log("valid");
+  submitButton.processing();
 });
