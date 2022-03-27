be6669de80c341672f659d62e8284c3089da09c0
Eden Crow <731483+EdenCrow@users.noreply.github.com>
2022-03-27 15:29:42 +0100

    Squoosh.js: + Exit on found squoosh directory + Better logging

--- scripts/squoosh.js
+++ scripts/squoosh.js
@@ -1,43 +1,56 @@
 // Original script by alex-drocks, taken from https://github.com/GoogleChromeLabs/squoosh/pull/1053#issuecomment-860072289
 
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
 
-const {ImagePool} = require('@squoosh/lib');
+const { ImagePool } = require("@squoosh/lib");
 const imagePool = new ImagePool();
 
 // Define your paths:
 const basePath = process.argv[2];
-const imagesToOptimizePath = path.join(basePath, "src")
+const imagesToOptimizePath = path.join(basePath, "src");
 const optimizeToPath = path.join(basePath, "squoosh");
 
 // Make the optimizeToPath directory if it does not exist
-if(!fs.existsSync(optimizeToPath)) {
+if (!fs.existsSync(optimizeToPath)) {
   fs.mkdirSync(optimizeToPath);
-  console.log("Created squoosh directory");
+  console.log(`Created squoosh directory ${optimizeToPath}`);
+} else {
+  console.log(
+    `Already squooshed files in ${basePath}. Run clean first to squoosh newly added files.`
+  );
+  process.exit(0);
 }
 
 // Define resize width & quality option
 const usrWidth = 1080;
 const usrQual = 75;
 
-(async () => {
-
+async function squooshFiles() {
+  let amount = 0
   const files = await fs.promises.readdir(imagesToOptimizePath);
-
   for (const file of files) {
     await libSquooshOptimize(file);
+    amount++;
   }
-
   await imagePool.close();
+  return amount;
+}
 
+(async () => {
+  console.log("Squooshing... ")
+  squooshFiles().then((amount) => {
+    console.log(`Successfully squooshed ${amount} files at ${basePath}`);
+  });
 })();
 
-
 async function libSquooshOptimize(file) {
+  process.stdout.write(path.parse(file).name);
   const imagePath = path.join(imagesToOptimizePath, file);
-  const saveOptimizedImageToPath = path.join(optimizeToPath, path.parse(file).name);
-  console.log({imagePath});
+  const saveOptimizedImageToPath = path.join(
+    optimizeToPath,
+    path.parse(file).name
+  );
 
   const image = imagePool.ingestImage(imagePath);
   await image.decoded; //Wait until the image is decoded before running preprocessors
@@ -46,24 +59,24 @@ async function libSquooshOptimize(file) {
     resize: {
       enabled: true,
       width: usrWidth,
-    }
+    },
   };
   await image.preprocess(preprocessOptions);
 
   const encodeOptions = {
     //an empty object means 'use default settings'
     mozjpeg: {
-      quality: usrQual
-    }
+      quality: usrQual,
+    },
   };
   await image.encode(encodeOptions);
 
   for (const encodedImage of Object.values(image.encodedWith)) {
-    const {extension, binary} = (await encodedImage);
+    const { extension, binary } = await encodedImage;
     const optimizedImagePath = `${saveOptimizedImageToPath}.${extension}`;
     fs.writeFile(optimizedImagePath, binary, (err) => {
       if (err) throw err;
-      console.log({optimizedImagePath});
-    },);
+    });
   }
+  process.stdout.write( " [✓]\n" );
 }
