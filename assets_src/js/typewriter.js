f8d852e1d5a2efbdb975241b5d4c742ecd6294a3
Eden Crow <731483+EdenCrow@users.noreply.github.com>
2023-02-13 20:37:49 +0000

    Small style changes

--- assets_src/js/typewriter.js
+++ assets_src/js/typewriter.js
@@ -6,7 +6,8 @@ new Typewriter(app, {
     loop: true,
     strings: [
         "<i class='fa fa-heart' aria-hidden='true'></i> Love Tech",
-        "<i class='fa fa-cloud' aria-hidden='true'></i> Web Development"
+        "<i class='fa fa-cloud' aria-hidden='true'></i> Web Development",
+        "<i class='fa fa-home' aria-hidden='true'></i> Home Lab & Automation"
     ],
     autoStart: true,
     pauseFor: 750,
